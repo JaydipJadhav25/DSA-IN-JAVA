6013b3a078935911ba746594aa8c6e813b860b01
jaydipjadhav <jaydipdjadhav>
2025-07-16 07:09:08 +0530

    while loop

---
+++ loops/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+     System.out.println("...............Loops.............");
+     System.out.println("|| While Loop ||");
+     int  counter =0;
+    //  while (counter <=10) {
+    //     System.out.println(counter);
+    //     // ++counter;
+    //     counter++;
+    //  }
+
+     Scanner sc = new Scanner(System.in);
+     int input = sc.nextInt();
+    //  while (counter <= input) {
+    //      System.out.println(counter +" to "+ input);
+    //     // ++counter;
+    //     counter++;
+    //  }
+
+
+    //sun to natural numbers
+//    int sum = 0;
+//    int nNumber= 1;
+//    while(nNumber <= input){
+//     sum +=nNumber;
+//     System.out.println("number : " + nNumber+" and sum "+sum);
+//      nNumber++;
+   
+//    } 
+//    System.out.println("Sum of N natural Numbers : "+sum);
+
+
+//for loop
+
+
+//print 
+// ****
+// ****
+// ****
+// ****
+
+
+}
+}
