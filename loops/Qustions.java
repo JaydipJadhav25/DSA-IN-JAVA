5f35edd167b14500d105a0c9bff3a7bac0ddacbd
jaydipjadhav <jaydipdjadhav>
2025-07-16 17:36:03 +0530

    loops qustions

--- loops/Qustions.java
+++ loops/Qustions.java
@@ -42,19 +42,101 @@ public class Qustions {
 
         //4. factorial of any number
 
-        System.out.print("Enter the number ");
-        int  n= sc.nextInt();
+        // System.out.print("Enter the number ");
+        // int  n= sc.nextInt();
 //          num = sc.nextInt();
-        int  factorialNumber =1 ;
+        // int  factorialNumber =1 ;
         // for(int i = 2 ; i<=n; i++){
         //   factorialNumber  *= i;
         // }
 
-        while (n >= 1) {
-             factorialNumber *=n;
-             n--;
+        // while (n >= 1) {
+        //      factorialNumber *=n;
+        //      n--;
+        // }
+        // System.out.println("factroril of"+n+" is : "+factorialNumber);
+  
+         
+        //5.
+        // for(int i = 1; i<=30; i++){
+        //     //for 3 multiple
+        //     if(i%3 == 0 && i%5 == 0){
+        //         System.out.println("Fizz Buzz "+i);  
+        //     }
+        //     else if(i%3 == 0){
+        //         System.out.println("fizz "+i);
+        //     }else if(i%5 == 0 && i%5 == 0){
+        //         System.out.println("buzz "+i);
+        //     }
+        //     else{
+        //         System.out.println(i);
+        //     }
+        // }
+
+        //5 => in optimize way
+// 1
+// 2
+// fizz 3
+// 4
+// buzz 5
+// fizz 6
+// 7
+// 8
+// fizz 9
+// buzz 10
+// 11
+// fizz 12
+// 13
+// 14
+// Fizz Buzz 15
+// 16
+// 17
+// fizz 18
+// 19
+// buzz 20
+// fizz 21
+// 22
+// 23
+// fizz 24
+// buzz 25
+// 26
+// fizz 27
+// 28
+// 29
+// Fizz Buzz 30
+
+
+        int counter3=0;
+        int counter5=0;
+    for(int i =1; i<=30;i++ ){
+        counter3++;
+        counter5++;
+
+        //1.
+        // if(counter3 == 3){
+        //     System.out.println("Fiuzz");
+        //     counter3 = 0;
+        // }else if(counter5 ==5){
+        //     System.out.println("buzz");
+        //     counter5 = 0;
+        // }else{
+        //         System.out.println(i);
+        // }
+
+
+        //2.
+        if(counter3 !=3 && counter5 !=5){
+            System.out.println(i);
         }
-        System.out.println("factroril of"+n+" is : "+factorialNumber);
+        if(counter3 == 3){
+             System.out.println("Fiuzz");
+             counter3 = 0;
+          }
+          if(counter5 ==5){
+             System.out.println("buzz");
+              counter5 = 0;
+         }
+    }
 
 
 
