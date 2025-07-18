c800a0d9bc5acab3c19656b0078516ce3c1208ad
jaydipjadhav <jaydipdjadhav>
2025-07-18 09:23:34 +0530

    understnd how to run test cases on online complier

--- Demo.cpp
+++ Demo.cpp
@@ -1,12 +1,29 @@
 #include<iostream>
 using namespace std;
 
+
+void pattern1(int n ){
+     for(int i = 0 ; i <n; i++){
+        for(int j =0; j<n; j++){
+          cout<<" * ";
+        }
+        cout<<endl;
+     }
+}
+
+
 int main(){
 
-  cout<<"hello wrold"<<endl;
-   int a;
-   cin>>a;
-   cout<<"output : "<<a;
+  int t=0;
+  cin>>t;
+  // cin>>n;
+  // pattern1(n);
+  for(int i = 0; i<2; i++){
+    int n ;
+    cin>>n;
+    pattern1(n);
+    cout<<".............";
+  }
 
     return 0;
 }
