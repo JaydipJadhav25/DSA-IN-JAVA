7bf7b50ce5dcb029a2893f9479f02b567173f7c4
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2025-08-10 16:27:34 +0530

    update

--- basic-recursio/CheckStringPalidrom.java
+++ basic-recursio/CheckStringPalidrom.java
@@ -1,6 +1,9 @@
 public class CheckStringPalidrom {
 
     public static boolean print(int i ,String s , int n ){
+
+        System.out.println("string is : "+s);
+
         if(i >= n/2) return true;
 
         if(s.charAt(i) != s.charAt(n-i-1)){
@@ -26,12 +29,19 @@ public class CheckStringPalidrom {
 
     public static void main(String[] args) {
         String s  = "maadaam";
+           String s2 = "A man, a plan, a canal: Panama";
+           String s3 = "a.";
+        //  String result = s2.replaceAll("[ , : ]", "").toUpperCase(); // removes spaces and commas
+        //  String result = s3.replaceAll("[ , : . ]", "").toUpperCase(); // removes spaces and commas
+         String result = s.replaceAll("[^a-zA-Z0-9]", "").toUpperCase();
+         System.out.println(result);
         // System.out.println(s);
         //single pointer
-        System.out.println("Strig is palidrom 1: "+ print(0, s, s.length()));
+       System.out.println("Strig is palidrom 1: "+ print(0, result, result.length()));
         //two pointer
-        System.out.println("Strig is palidrom 2: "+ print2(0, s, (s.length())-1));
+       // System.out.println("Strig is palidrom 2: "+ print2(0, s, (s.length())-1));
 
 
+    
     }
 }
