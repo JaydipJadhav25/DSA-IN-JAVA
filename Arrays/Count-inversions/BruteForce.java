0319e8d2cad23385ab0b92bc2bf4a19023600d95
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2025-10-10 23:59:26 +0530

    count inversion

---
+++ Arrays/Count-inversions/BruteForce.java
@@ -0,0 +1,22 @@
+// package Count-inversions;
+
+public class BruteForce {
+    public static void main(String[] args) {
+        int[] a = {5, 4, 3, 2, 1};
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+             
+            for (int j = i+1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    count++;
+                }
+            }
+            
+        }
+
+
+        System.out.println("count : "+ count);
+        
+    }
+    
+}
